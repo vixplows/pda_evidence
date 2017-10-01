eee68a3ea9325ba8c9833c0d065b5a699a1c64d2
vixplows <vicky@vickyplows.co.uk>
2017-10-01 16:38:17 +0100

    Pass chain multiple operators test

--- coding_excercises/js_calculator/tests/integration/tests.js
+++ coding_excercises/js_calculator/tests/integration/tests.js
@@ -60,4 +60,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should be able to chain multiple operators', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10')
+  })
+
 });
